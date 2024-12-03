aa1e0c1214bc3bd60b21676e6416c45b5ffb3ff8
79650 <white.electric.dog@gmail.com>
2024-12-03 15:46:13 +0700

    start new project

---
+++ app/src/main/java/com/example/chatverse/ui/theme/MyProjectTheme.kt
@@ -0,0 +1,51 @@
+package com.example.chatverse.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+import androidx.compose.material3.Typography
+
+
+// Цветовая палитра для светлой темы
+private val LightColors = lightColorScheme(
+    primary = Color(0xFF6200EE),
+    onPrimary = Color.White,
+    secondary = Color(0xFF03DAC6),
+    onSecondary = Color.Black,
+    background = Color(0xFF121212),
+    onBackground = Color.White,
+    surface = Color(0xFF121212),
+    onSurface = Color.White
+)
+
+// Цветовая палитра для темной темы
+private val DarkColors = darkColorScheme(
+    primary = Color(0xFFBB86FC),
+    onPrimary = Color.Black,
+    secondary = Color(0xFF03DAC6),
+    onSecondary = Color.Black,
+    background = Color(0xFF121212),
+    onBackground = Color.White,
+    surface = Color(0xFF121212),
+    onSurface = Color.White
+)
+
+// Функция для применения темы
+@Composable
+fun MyProjectTheme(
+    darkTheme: Boolean = false, // Выбор между светлой и темной темой
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) DarkColors else LightColors
+
+    MaterialTheme(
+        colorScheme = colors,
+        typography = Typography(),
+        content = content
+    )
+}
+
+
